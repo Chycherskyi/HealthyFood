0692b19c0a573791d5c71aaa32338ea9e2aa0cda
olehchycherskyi <oleg.czyczerski@gmail.com>
2021-07-07 16:46:27 +0200

    Splitting code into modules webpack

---
+++ js/modules/tabs.js
@@ -0,0 +1,38 @@
+function tabs(){
+    //Tabs
+
+     const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader__items'),
+        tabsContent = document.querySelectorAll('.tabcontent');
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabsConten(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsConten();
+
+    tabsParent.addEventListener('click', (event) => {
+        target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabsConten(i);
+                }
+            });
+        }
+    });
+}
+
+module.exports = tabs;
